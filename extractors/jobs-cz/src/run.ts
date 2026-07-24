527c642675ce4fa28c9c81df94109d6a58c4f19e
Yurii Kuvshynov <141632421+fogrye@users.noreply.github.com>
2026-07-24 08:15:23 +0200

    fix: decode numeric HTML entities in Jobs.cz descriptions

--- extractors/jobs-cz/src/run.ts
+++ extractors/jobs-cz/src/run.ts
@@ -71,9 +71,12 @@ function decodeHtml(value: string): string {
     .replace(/&nbsp;/gi, " ")
     .replace(/&amp;/gi, "&")
     .replace(/&quot;/gi, '"')
-    .replace(/&#39;/gi, "'")
     .replace(/&lt;/gi, "<")
-    .replace(/&gt;/gi, ">");
+    .replace(/&gt;/gi, ">")
+    .replace(/&#x([0-9a-f]+);/gi, (_, code: string) =>
+      String.fromCodePoint(Number.parseInt(code, 16)),
+    )
+    .replace(/&#(\d+);/g, (_, code: string) => String.fromCodePoint(Number(code)));
 }
 
 function stripHtml(value: string): string {
